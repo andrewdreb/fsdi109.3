ae44577a05731ac9e1211582691126aed93b9771
andrewdreb <andrew.drebenstedt@sdgku.edu>
2024-12-06 00:00:10 -0600

    Updated project files and structure

--- online-store-52/src/components/ShoppingList.js
+++ online-store-52/src/components/ShoppingList.js
@@ -2,18 +2,42 @@ import React, { useState } from 'react';
 import Item from './Item';
 
 const initialItems = [
-  { id: 1, name: 'Item 1', price: 10 },
-  { id: 2, name: 'Item 2', price: 20 },
-  { id: 3, name: 'Item 3', price: 30 },
-  { id: 4, name: 'Item 4', price: 40 },
-  { id: 5, name: 'Item 5', price: 50 },
-  { id: 6, name: 'Item 6', price: 60 },
-  { id: 7, name: 'Item 7', price: 70 },
-  { id: 8, name: 'Item 8', price: 80 },
-  { id: 9, name: 'Item 9', price: 90 },
-  { id: 10, name: 'Item 10', price: 100 },
-  { id: 11, name: 'Item 11', price: 110 },
-  { id: 12, name: 'Item 12', price: 120 },
+  { id: 1,
+     name: 'Item 1', 
+     price: 10 },
+  { id: 2,
+     name: 'Item 2', 
+     price: 20 },
+  { id: 3,
+     name: 'Item 3', 
+     price: 30 },
+  { id: 4,
+     name: 'Item 4', 
+     price: 40 },
+  { id: 5,
+     name: 'Item 5', 
+     price: 50 },
+  { id: 6,
+     name: 'Item 6', 
+     price: 60 },
+  { id: 7,
+     name: 'Item 7', 
+     price: 70 },
+  { id: 8,
+     name: 'Item 8', 
+     price: 80 },
+  { id: 9,
+     name: 'Item 9', 
+     price: 90 },
+  { id: 10,
+     name: 'Item 10', 
+     price: 100 },
+  { id: 11,
+     name: 'Item 11', 
+     price: 110 },
+  { id: 12,
+     name: 'Item 12', 
+     price: 120 },
 ];
 
 const ShoppingList = ({ addItemToCart }) => {
@@ -32,6 +56,6 @@ const ShoppingList = ({ addItemToCart }) => {
     </div>
   );
 };
-adfdfg..gadfg
+
 
 export default ShoppingList;
